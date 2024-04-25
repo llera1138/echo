ff8da86458693459c86e4fd9630e965da2e29d60
LERA <llera1138@yandex.ru>
2024-04-25 22:16:00 +0300

    change

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        var text = "Hi";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(text));
+    }
+}
